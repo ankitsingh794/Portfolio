f8529b29c953a8ec4855280fcac931e28388c264
Ankit Singh <ankitsinghrjt794@gmail.com>
2025-07-02 18:16:53 +0530

    Initial Commit

--- src/pages/Introduction.jsx
+++ src/pages/Introduction.jsx
@@ -1,23 +1,41 @@
 import React from "react";
 import ProfileCard from "../components/ProfileCard";
-import "./Introduction.css"; 
+import ShinyText from "../components/ShinyText";
+import "./Introduction.css";
 
 export const Introduction = () => {
     return (
-        <div className="introduction">
-            <ProfileCard
-                name="Ankit Singh"
-                title="Software Engineer"
-                handle="ankitsingh794"
-                status="Online"
-                contactText="Contact Me"
-                avatarUrl="https://res.cloudinary.com/divulwxho/image/upload/v1751456065/WhatsApp_Image_2025-07-02_at_17.00.05_63987d7e-removebg-preview_mntuxp.png"
-                showUserInfo={true}
-                enableTilt={true}
-                onContactClick={() => console.log('Contact clicked')}
-            />
+        <div className="introduction" >
+            
+            <div className="intro-text">
+                <ShinyText
+                    text="Hey fam! Welcome to my Portfolio."
+                    disabled={false}
+                    speed={3}
+                    className="custom-class"
+                />
+            </div>
+
+            <div className="profile-section">
+                <ProfileCard
+                    name="Ankit Singh"
+                    title="Software Engineer"
+                    handle="ankitsingh794"
+                    status="Online"
+                    contactText="Contact Me"
+                    avatarUrl="https://res.cloudinary.com/divulwxho/image/upload/v1751456065/WhatsApp_Image_2025-07-02_at_17.00.05_63987d7e-removebg-preview_mntuxp.png"
+                    showUserInfo={true}
+                    enableTilt={true}
+                    onContactClick={() => console.log('Contact clicked')}
+                />
+            </div>
+
+            <div className="contact-info" >
+                Feel free to connect or explore more below.
+            </div>
+
         </div>
     );
-}
+};
 
 export default Introduction;
